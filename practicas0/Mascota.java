6fbd6530e73ffeb2da3918c04f20421ea317e70a
Angel Fernandez <angel_mirras@hotmail.com>
2024-01-29 00:31:20 +0100

    añadimos codigo, variables constructor getters y setters.

--- practicas0/Mascota.java
+++ practicas0/Mascota.java
@@ -1,5 +1,46 @@
 package practicas0;
 
 public class Mascota {
+	private int numPatas;
+	private String nombre;
+	private String voz;
+	private double peso;
+	
+	public Mascota(int numPatas, String nombre, String voz, double peso) {
+		this.setNumPatas(numPatas);
+		this.setNombre(nombre);
+		this.setVoz(voz);
+		this.peso = peso;
+	}
+
+	public int getNumPatas() {
+		return numPatas;
+	}
+
+	public void setNumPatas(int numPatas) {
+		this.numPatas = numPatas;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getVoz() {
+		return voz;
+	}
+
+	public void setVoz(String voz) {
+		this.voz = voz;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+	
+	
 
 }
